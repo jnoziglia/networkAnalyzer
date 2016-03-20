5f3c03d8fe5f53d28c6ee10d5e2176e9e0154ec1
Julian Noziglia <julian.noziglia@gmail.com>
2016-03-20 01:21:07 -0300

    Mini programa de python de prueba

---
+++ src/Analyzer.py
@@ -0,0 +1,7 @@
+import subprocess as sub
+
+class Analyzer:
+    p = sub.Popen(('sudo', 'tcpdump', '-nnvvXSs', '1514', '-r', '../output'), stdout=sub.PIPE)
+    for line in iter(p.stdout.readline, b''):
+        print ('TEST')
+
