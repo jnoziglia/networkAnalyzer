cc9cfdabbc634d1c8d7c042ede6a54a7a895883b
Julian Noziglia <julian.noziglia@gmail.com>
2016-03-20 00:07:58 -0300

    Modifico script

--- script1.sh
+++ script1.sh
@@ -2,6 +2,6 @@
 
 for i in {1..10}
 do
-	nc -N 192.168.1.9 1234 < dummy1.html
+	sendip -p ipv4 -is 192.168.1.10 -id 192.168.1.9 -p udp -us 5070 -ud 5060 -d "Hello" -v 192.168.1.9
 	sleep $(( ( RANDOM % 10 ) + 1 ))
 done
