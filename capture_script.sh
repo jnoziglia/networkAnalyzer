c1a014cbe5c701ba3f1b2be8076506bc22fb8955
Julian Noziglia <julian.noziglia@gmail.com>
2016-03-21 00:45:41 -0300

    Corrijo scripts

--- capture_script.sh
+++ capture_script.sh
@@ -2,4 +2,4 @@
 
 . ./ip_config.cfg
 
-tcpdump -i eth0 dst "$ip_1" or dst "$ip_2" not arp and not rarp -w - | ssh ubuntu@"$ip_main" 'cat >> output_new'
+tcpdump -i eth0 dst "$ip_1" or dst "$ip_2" -w - | ssh ubuntu@"$ip_main" 'cat >> $my_ip output'
--- network_script_1.sh
+++ network_script_1.sh
@@ -4,8 +4,8 @@
 
 for i in {1..20}
 do
-	sendip -p ipv4 -id "$ip_1" -p udp -us 5000 -ud 5000 -d "Hello" -v "$ip_1"
-	sendip -p ipv4 -id "$ip_2" -p udp -us 5000 -ud 5000 -d "Hello" -v "$ip_2"
+	sendip -p ipv4 -is "$my_ip" -id "$ip_1" -p udp -us 5000 -ud 5000 -d "Hello" -v "$ip_1"
+	sendip -p ipv4 -is "$my_ip" -id "$ip_2" -p udp -us 5000 -ud 5000 -d "Hello" -v "$ip_2"
 	sleep $(( ( RANDOM % 5 ) + 1 ))
 done
 echo "Finished"
