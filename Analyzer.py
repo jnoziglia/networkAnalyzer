118a6f85c97fe4a0bd8a574b1b983904ce535713
Julian Noziglia <julian.noziglia@gmail.com>
2016-03-22 06:35:30 +0000

    Pruebas

--- Analyzer.py
+++ Analyzer.py
@@ -1,18 +1,21 @@
 import subprocess as sub
+import re
 
 class Analyzer:
     sub.call("./create_capture.sh")
-    p = sub.Popen(('sudo', 'tcpdump', '-nn', '-r', './output'), stdout=sub.PIPE)
+    p = sub.Popen(('sudo', 'tcpdump', 'ip', '-l', '-nn', '-r', './output'), stdout=sub.PIPE)
 
-    import re
-
-    reg = re.compile(r"(?P<timestamp>(?:\d{1,2}\:){2}\d{1,2}\.\d{1,6}) IP (?P<IP1>(?:\d{1,3}\.){3}\d{1,3})\.(?P<Port1>\d+) > (?P<IP2>(?:\d{1,3}\.){3}\d{1,3})\.(?P<Port2>\d+): (?P<protocol>(?:tcp|udp|icmp))")
-
+    reg = re.compile("(?P<timestamp>(?:\d{1,2}\:){2}\d{1,2}\.\d{1,6}) IP (?P<IP1>(?:\d{1,3}\.){3}\d{1,3})\.(?P<Port1>\d+) > (?P<IP2>(?:\d{1,3}\.){3}\d{1,3})\.(?P<Port2>\d+): (?P<protocol>(?:TCP|UDP|ICMP))")
+    i = 0
+    
     for line in iter(p.stdout.readline, b''):
         m = reg.match(line)
-        print(m.group("timestamp"))
-        print(m.group("IP1"))
-        print(m.group("Port1"))
-        print(m.group("IP2"))
-        print(m.group("Port2"))
-        print(m.group("protocol"))
+	print i
+        #print(m.group("timestamp"))
+        #print(m.group("IP1"))
+        #print(m.group("Port1"))
+        #print(m.group("IP2"))
+        #print(m.group("Port2"))
+        #print(m.group("protocol"))
+	print line
+	i = i+1
