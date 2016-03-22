b8dbd3452e9c7a4c1b0196f0ba73f050188305b1
jnoziglia <julian.noziglia@gmail.com>
2016-03-22 12:32:29 -0300

    Pruebo con otra regex

--- Analyzer.py
+++ Analyzer.py
@@ -5,25 +5,23 @@ class Analyzer:
     sub.call("./create_capture.sh")
     #p = sub.Popen(('sudo', 'tcpdump', 'ip', '-l', '-nn', '-r', './output'), stdout=sub.PIPE)
 
-    regex_string = r'(?P<timestamp>(?:\d{1,2}\:){2}\d{1,2}\.\d{1,6}) IP (?P<IP1>(?:\d{1,3}\.){3}\d{1,3})\.(?P<Port1>\d+) > (?P<IP2>(?:\d{1,3}\.){3}\d{1,3})\.(?P<Port2>\d+): (?P<protocol>(?:TCP|UDP|ICMP))'
+    # regex_string = r'(?P<timestamp>(?:\d{1,2}\:){2}\d{1,2}\.\d{1,6}) IP (?P<IP1>(?:\d{1,3}\.){3}\d{1,3})\.(?P<Port1>\d+) > (?P<IP2>(?:\d{1,3}\.){3}\d{1,3})\.(?P<Port2>\d+): (?P<protocol>(?:TCP|UDP|ICMP))'
 
-    reg = re.compile(regex_string)
-    # i = 0
+    reg = re.compile('\d+')
 
     with open('outputtxt') as output:
         for line in output:
-            m = re.search(regex_string, line).groups()
-	    print line
-            print(m.group("timestamp"))
+            m = reg.match(line)
+            print line
+            print m.group()
 
-    # #for line in iter(p.stdout.readline, b''):
+    # for line in iter(p.stdout.readline, b''):
     #     m = reg.match(line)
     #     print i
-    #     #print(m.group("timestamp"))
-    #     #print(m.group("IP1"))
-    #     #print(m.group("Port1"))
-    #     #print(m.group("IP2"))
-    #     #print(m.group("Port2"))
-    #     #print(m.group("protocol"))
+    #     print(m.group("timestamp"))
+    #     print(m.group("IP1"))
+    #     print(m.group("Port1"))
+    #     print(m.group("IP2"))
+    #     print(m.group("Port2"))
+    #     print(m.group("protocol"))
     #     print line
-    #     i = i+1
