13ae8b9e7fb8e19f0c2877efae92d5cd6022eaec
Julian Noziglia <julian.noziglia@gmail.com>
2016-03-22 15:25:43 +0000

    abro archivo txt

--- Analyzer.py
+++ Analyzer.py
@@ -3,7 +3,7 @@ import re
 
 class Analyzer:
     sub.call("./create_capture.sh")
-    p = sub.Popen(('sudo', 'tcpdump', 'ip', '-l', '-nn', '-r', './output'), stdout=sub.PIPE)
+    #p = sub.Popen(('sudo', 'tcpdump', 'ip', '-l', '-nn', '-r', './output'), stdout=sub.PIPE)
 
     regex_string = r'(?P<timestamp>(?:\d{1,2}\:){2}\d{1,2}\.\d{1,6}) IP (?P<IP1>(?:\d{1,3}\.){3}\d{1,3})\.(?P<Port1>\d+) > (?P<IP2>(?:\d{1,3}\.){3}\d{1,3})\.(?P<Port2>\d+): (?P<protocol>(?:TCP|UDP|ICMP))'
 
@@ -12,7 +12,8 @@ class Analyzer:
 
     with open('outputtxt') as output:
         for line in output:
-            m = reg.match(line)
+            m = re.search(regex_string, line).groups()
+	    print line
             print(m.group("timestamp"))
 
     # #for line in iter(p.stdout.readline, b''):
