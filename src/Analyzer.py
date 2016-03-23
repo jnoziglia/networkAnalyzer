0fc89dbe0cf7a4adc769d124c2d0740c116dff72
Julian Noziglia <julian.noziglia@gmail.com>
2016-03-23 07:00:11 +0000

    Lee protocolo IP

--- src/Analyzer.py
+++ src/Analyzer.py
@@ -6,12 +6,14 @@ regex_ip = r'(?P<timestamp>(?:\d{1,2}\:){2}\d{1,2}\.\d{1,6}) IP (?P<IP1>(?:\d{1,
 
 class Analyzer:
 
-    def process_ip(self, line):
+    def process_ip(line):
         reg = re.compile(regex_ip)
         m = reg.match(line)
         print(m.group('IP1'))
+	print(m.group('protocol'))
+	print('--------------------')
 
-    sub.call("./create_capture.sh")
+    #sub.call("./create_capture.sh")
     p = sub.Popen(('sudo', 'tcpdump', '-l', '-nn', '-r', '../output'), stdout=sub.PIPE)
 
     # regex_string = r'(?P<timestamp>(?:\d{1,2}\:){2}\d{1,2}\.\d{1,6}) IP (?P<IP1>(?:\d{1,3}\.){3}\d{1,3})\.(?P<Port1>\d+) > (?P<IP2>(?:\d{1,3}\.){3}\d{1,3})\.(?P<Port2>\d+): (?P<protocol>(?:TCP|UDP|ICMP))'
