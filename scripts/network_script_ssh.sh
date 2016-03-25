9cf6b0d968a48baf4be57175752f8c64d45f1856
Julian Noziglia <julian.noziglia@gmail.com>
2016-03-25 23:21:25 +0000

    Corrijo script ssh

--- scripts/network_script_ssh.sh
+++ scripts/network_script_ssh.sh
@@ -2,5 +2,5 @@
 
 . ../ip_config.cfg
 
-scp ./dummy1.html ubuntu@"$ip_1":~/dummy.html
-scp ./dummy1.html ubuntu@"$ip_2":~/dummy.html
+scp ../dummy1.html ubuntu@"$ip_1":~/dummy.html
+scp ../dummy1.html ubuntu@"$ip_2":~/dummy.html
