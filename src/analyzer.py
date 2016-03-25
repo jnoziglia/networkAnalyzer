d4a9d0297cdb94961ae31d539f3717018e37354c
Julian Noziglia <julian.noziglia@gmail.com>
2016-03-25 02:53:28 -0300

    Parseo ips y puertos

--- src/analyzer.py
+++ src/analyzer.py
@@ -10,28 +10,41 @@ from yattag import indent
 
 class Analyzer(object):
     regex_ip_1 = r'IP (?:\(tos \w*, ttl \d*, id (?P<id>\d*), offset (?P<offset>\d*), flags \[\w*\], proto (?P<transport_protocol>\w*) (?:\(\d*\))?, length (?P<length>\d*)\))'
-    # regex_ip_2 = r'(?P<IP1>(?:\d{1,3}\.){3}\d{1,3})\.?(?P<Port1>\d+)? > (?P<IP2>(?:\d{1,3}\.){3}\d{1,3})\.?(?P<Port2>\d+)?: (?P<transport_protocol>TCP|UDP|ICMP)'
+    regex_ip_2 = r'(?P<src>(?:\d{1,3}\.){3}\d{1,3})\.?(?P<src_port>\d*) > (?P<dst>(?:\d{1,3}\.){3}\d{1,3})\.?(?P<dst_port>\d*): (?P<protocol>\w+)'
     regex_timestamp = r'(?P<timestamp>(?:\d{1,2}\:){2}\d{1,2}\.\d{1,6}) (?P<protocol>IP|ARP)'
+    regex_port_error = r'port \w+ unreachable'
     reg_ip_1 = re.compile(regex_ip_1)
+    reg_ip_2 = re.compile(regex_ip_2)
     reg_timestamp = re.compile(regex_timestamp)
+    reg_port_error = re.compile(regex_port_error)
     events = []
 
     # sub.call("./create_capture.sh")
     doc, tag, text = Doc().tagtext()
-    events_empty = 1
+    add_event = 0
     p = sub.Popen(('sudo', 'tcpdump', 'ip', '-l', '-nnv', '-r', '../output'), stdout=sub.PIPE)
     for line in iter(p.stdout.readline, b''):
         m = reg_timestamp.match(line.decode())
         if m:
-            if events_empty == 0:
+            if add_event == 1:
                 events.append(event)
-            events_empty = 0
+            add_event = 1
             event = Event(m.group('timestamp'), m.group('protocol'))
             m = reg_ip_1.search(line.decode())
             if m:
                 event.id = m.group('id')
                 event.t_protocol = m.group('transport_protocol')
                 event.length = m.group('length')
+        else:
+            m = reg_ip_2.match(line)
+            if m:
+                event.src = m.group('src')
+                event.src_port = m.group('src_port')
+                event.dst = m.group('dst')
+                event.dst_port = m.group('dst_port')
+            m = reg_port_error.match(line)
+            if m:
+                add_event = 0
 
     for event in events:
         with tag ('tr'):
