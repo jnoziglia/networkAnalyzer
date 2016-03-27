3cece1b99e12470258e188d7bda62f6180b39e15
Julian <julian.noziglia@gmail.com>
2016-03-27 20:27:46 -0300

    Agrego opciones a la captura

--- scripts/capture_script.sh
+++ scripts/capture_script.sh
@@ -2,4 +2,4 @@
 
 . ../config/ip_config.cfg
 
-tcpdump -i eth0 src "$ip_1" or src "$ip_2" or src "$ip_3" -w ../captures/"$my_ip"
+tcpdump -i eth0 -s0 -U src "$ip_1" or src "$ip_2" or src "$ip_3" -w ../captures/"$my_ip"
