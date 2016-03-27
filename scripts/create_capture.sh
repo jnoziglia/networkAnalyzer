127cb07f42345d1b1d12843143c806ff7db50470
Julian Noziglia <julian.noziglia@gmail.com>
2016-03-27 17:07:43 -0300

    Corrijo script para capturar. Creo script para realizar capturas desde todas las pcs

--- scripts/create_capture.sh
+++ scripts/create_capture.sh
@@ -3,7 +3,8 @@
 . ../ip_config.cfg
 
 sftp ubuntu@"$ip_1":/home/ubuntu/networkAnalyzer/captures/"$ip_1"
-sftp ubuntu@"$ip_1":/home/ubuntu/networkAnalyzer/captures/"$ip_2"
-sftp ubuntu@"$ip_1":/home/ubuntu/networkAnalyzer/captures/"$ip_3"
-mergecap ./"$ip_1" ./"$ip_2" ./"$ip_3" -w ../output
+sftp ubuntu@"$ip_2":/home/ubuntu/networkAnalyzer/captures/"$ip_2"
+sftp ubuntu@"$ip_3":/home/ubuntu/networkAnalyzer/captures/"$ip_3"
+sftp ubuntu@"$ip_4":/home/ubuntu/networkAnalyzer/captures/"$ip_4"
+mergecap ./"$ip_1" ./"$ip_2" ./"$ip_3" ./"$ip_4" -w ../output
 tcpdump -nnv -r ../output > ../outputtxt
---
+++ scripts/run_network_scripts.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+. ../ip_config.cfg
+
+ssh ubuntu@"$ip_1" '~/networkAnalyzer/scripts/capture_script.sh'
+ssh ubuntu@"$ip_2" '~/networkAnalyzer/scripts/capture_script.sh'
+ssh ubuntu@"$ip_3" '~/networkAnalyzer/scripts/capture_script.sh'
+ssh ubuntu@"$ip_4" '~/networkAnalyzer/scripts/capture_script.sh'
+ssh ubuntu@"$ip_1" '~/networkAnalyzer/scripts/network_script_1.sh'
+ssh ubuntu@"$ip_2" '~/networkAnalyzer/scripts/network_script_1.sh'
+ssh ubuntu@"$ip_3" '~/networkAnalyzer/scripts/network_script_1.sh'
+ssh ubuntu@"$ip_4" '~/networkAnalyzer/scripts/network_script_1.sh'
+ssh ubuntu@"$ip_1" '~/networkAnalyzer/scripts/network_script_scp.sh'
+ssh ubuntu@"$ip_2" '~/networkAnalyzer/scripts/network_script_scp.sh'
+ssh ubuntu@"$ip_3" '~/networkAnalyzer/scripts/network_script_scp.sh'
+ssh ubuntu@"$ip_4" '~/networkAnalyzer/scripts/network_script_scp.sh'
+ssh ubuntu@"$ip_1" '~/networkAnalyzer/scripts/network_script_rcp.sh'
+ssh ubuntu@"$ip_2" '~/networkAnalyzer/scripts/network_script_rcp.sh'
+ssh ubuntu@"$ip_3" '~/networkAnalyzer/scripts/network_script_rcp.sh'
+ssh ubuntu@"$ip_4" '~/networkAnalyzer/scripts/network_script_rcp.sh'
