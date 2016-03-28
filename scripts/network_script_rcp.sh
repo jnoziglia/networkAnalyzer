d2b1270faffefc8fc6f04e6350eb9737df9a9513
Julian <julian.noziglia@gmail.com>
2016-03-27 22:41:41 -0300

    Corrijo sleep

--- scripts/network_script_rcp.sh
+++ scripts/network_script_rcp.sh
@@ -7,6 +7,6 @@ do
 	rcp ~/networkAnalyzer/resources/dummy1.html ubuntu@"$ip_1":~/dummy.html >/dev/null 2>&1
 	rcp ~/networkAnalyzer/resources/dummy1.html ubuntu@"$ip_2":~/dummy.html >/dev/null 2>&1
 	rcp ~/networkAnalyzer/resources/dummy1.html ubuntu@"$ip_3":~/dummy.html >/dev/null 2>&1
-	sleep $(( ( RANDOM % 3 ) + 1 ))
+	sleep $(( ( RANDOM % 5 ) + 1 ))
 done
 echo "Finished"
